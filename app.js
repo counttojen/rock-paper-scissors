a03c7bcbb250f295d56f0a917f44ad7a5dcbd5d8
counttojen <jenniferc9912@gmail.com>
2025-11-19 18:27:48 -0500

    Add logic for UI elements and styles and change game logic
    Game logic now ends after one player reaches 5 points

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const rockBtn = document.createElement("button");
 const paperBtn = document.createElement("button");
 const scissorsBtn = document.createElement("button");
+const body = document.querySelector("body");
 const btnContainer = document.querySelector(".btnContainer")
+const scoreContainer = document.createElement("div");
+const humanChoiceDisplay = document.createElement("h3");
+const computerChoiceDisplay = document.createElement("h3");
+const roundDisplay = document.createElement("h3");
+const gameWinner = document.createElement("h3");
+const workspaceContainer = document.createElement("div");
+const scores = document.createElement("h3");
+
+body.appendChild(workspaceContainer);
 
 
-
-rockBtn.textContent = "Rock";
-paperBtn.textContent = "Paper";
-scissorsBtn.textContent = "Scissors";
+rockBtn.textContent = "Rock".toUpperCase();
+paperBtn.textContent = "Paper".toUpperCase();
+scissorsBtn.textContent = "Scissors".toUpperCase();
 
 btnContainer.append(rockBtn, paperBtn, scissorsBtn);
 const rpsBtns = btnContainer.querySelectorAll("button");
 
 
+//btn styles 
+rpsBtns.forEach(btn =>{
+    Object.assign(btn.style,{
+        backgroundColor: "#ffffff",
+        border: "none",
+        margin: "8px",
+        padding: "10px 20px",
+        borderRadius: "10px",
+        fontFamily: "Elms Sans",
+        fontSize: "20px",
+    })
+})
+
+
 function getHumanChoice(btn){
     let humanChoice = btn.textContent;
-    console.log(`Human choice: ${humanChoice}`);
+    console.log(`Your choice: ${humanChoice}`);
     return humanChoice;
 }
 
@@ -23,14 +46,63 @@ function getHumanChoice(btn){
 function getComputerChoice(){
     let computerChoiceArray = ["Rock", "Paper", "Scissors"];
     let randomNumber = Math.floor(Math.random() * computerChoiceArray.length);
-    let computerChoice = computerChoiceArray[randomNumber];
+    let computerChoice = computerChoiceArray[randomNumber].toUpperCase();
     console.log(`Computer choice: ${computerChoice}`);
     return computerChoice;
 }
 
+
+
+workspaceContainer.appendChild(scoreContainer);
+workspaceContainer.prepend(btnContainer);
+workspaceContainer.prepend(roundDisplay);
+workspaceContainer.append(scores);
+
 let humanScore = 0;
 let computerScore = 0;
 let round = 0;
+
+//body styles 
+Object.assign(body.style,{
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Elms Sans",
+    marginTop: "70px",
+})
+
+//workspaceContainer styles
+Object.assign(workspaceContainer.style,{
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ffccee",
+    borderRadius: "20px",
+    width: "400px",
+    padding: "20px",
+    boxShadow: "10px 6px 0 0 orange, -10px -6px 0 0 purple",
+})
+
+
+scoreContainer.append(humanChoiceDisplay, computerChoiceDisplay);
+
+//scoreContainer styles
+Object.assign(scoreContainer.style, {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+})
+
+
+humanChoiceDisplay.textContent = `Your Choice: `;
+computerChoiceDisplay.textContent = `Computer Choice: `;
+roundDisplay.textContent = `Round: ${round}`;
+scores.textContent = `Computer Score: ${computerScore}, Your Score: ${humanScore}`;
+
+
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
@@ -38,25 +110,38 @@ function playRound(humanChoice, computerChoice){
         return "DRAW";
     }
     else if(
-        (humanChoice === "Rock" && computerChoice === "Paper")||
-        (humanChoice === "Scissors" && computerChoice === "Rock")||
-        (humanChoice === "Paper" && computerChoice === "Scissors")){
+        (humanChoice === rockBtn.textContent && computerChoice === paperBtn.textContent)||
+        (humanChoice === scissorsBtn.textContent && computerChoice === rockBtn.textContent)||
+        (humanChoice === paperBtn.textContent && computerChoice === scissorsBtn.textContent)){
         computerScore += 1;
-        console.log(`Computer Score: ${computerScore}, Human Score: ${humanScore}.`);
-        return computerScore;
+        console.log(`Computer Score: ${computerScore}, Your Score: ${humanScore}.`);
     }
     else {
         humanScore += 1;
-        console.log(`Computer Score: ${computerScore}, Human Score: ${humanScore}`);
-        return humanScore;
+        console.log(`Computer Score: ${computerScore}, Your Score: ${humanScore}`);
     }
 }
 
 
+rpsBtns.forEach(btn =>{
+    btn.addEventListener("mouseover", ()=>{
+        Object.assign(btn.style,{
+            backgroundColor: "orange",
+            color: "#ffffff",
+            cursor: "pointer",
+        })
+    btn.addEventListener("mouseout", ()=>{
+        Object.assign(btn.style,{
+            backgroundColor: "#ffffff",
+            color: "#000000",
+        })
+    })
+    })
+})
 
 rpsBtns.forEach(btn =>{
     btn.addEventListener("click", ()=>{
-        if(round === 5){
+        if(humanScore === 5 || computerScore === 5){
         return;
         }
         round += 1 
@@ -65,16 +150,36 @@ rpsBtns.forEach(btn =>{
         let humanChoice = getHumanChoice(btn)
         let computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice);
-        
+
+        humanChoiceDisplay.textContent = `Your Choice: ${humanChoice}`;
+        computerChoiceDisplay.textContent = `Computer Choice: ${computerChoice}`;
+        roundDisplay.textContent = `Round: ${round}`;
+        scores.textContent = `Computer Score: ${computerScore}, Your Score: ${humanScore}`;
+
+
+        if (humanScore === 5 || computerScore === 5){
+            scoreContainer.append(gameWinner);
         if(humanScore > computerScore){
-            console.log("HUMAN WINS!")
+            gameWinner.textContent = `YOU WIN!`;
+            
         }
         else if(humanScore === computerScore){
-            console.log("DRAW!");
+            gameWinner.textContent = "DRAW!";
+            
         }
         else{
-            console.log("COMPUTER WINS!")
+            gameWinner.textContent = `COMPUTER WINS!`;
+            
         }
+            Object.assign(gameWinner.style,{
+                backgroundColor: "orange",
+                width: "fit-content",
+                padding: "10px 20px",
+                color: "white",
+                borderRadius: "10px",
+                fontSize: "30px",
+            })
+    }
     }
 )})
 
